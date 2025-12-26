7224804a3f13a7a942f859d38ced77147c898b43
Camilo Valencia <camival1999@gmail.com>
2025-12-26 04:12:51 +0100

    Changes tree cleanup

--- firmware/.pio/build/esp32s3/FrameworkArduino/firmware_msc_fat.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32s3/FrameworkArduino/firmware_msc_fat.c.o: \
- C:/Users/camiv/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
- C:/Users/camiv/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
